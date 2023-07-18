8699d8ac21f97796281a8f1319fd5f6bdc8f07b2
david <davidemv1622@gmail.com>
2023-07-18 15:45:23 -0500

    Correcion comentarios

--- lib/src/pages/Login_page.dart
+++ lib/src/pages/Login_page.dart
@@ -145,39 +145,4 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-
-  /*
-  // ---- Widget para el campo de texto "Correo" ----
-  Widget _textFieldEmail(String textoCorreo) {
-    return Container(
-      // Permite definir el diseño de la caja de texto "BoxDecoration"
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(217, 217, 217, 1), // Color del input
-        borderRadius:
-            BorderRadius.circular(10.0), // Define los bordes redondeados
-        border: Border.all(
-          // Define el grosor y color de borde
-          color: Colors.black,
-          width: 1.0,
-        ),
-      ),
-      // Espaciado entre el borde y el contenido
-      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-      //margin: EdgeInsets.symmetric(horizontal: 10.0),
-
-      // Uso de un input (Campo de texto)
-      child: TextField(
-          style: TextStyle(fontSize: 25), // Tamanio del texto
-          decoration: InputDecoration.collapsed(
-            /* Quita la linea que viene por 
-                                                  defecto en el input "InputDecoration.collapsed" */
-            hintText: textoCorreo, // Place holder en el input
-          )),
-      /*
-          height: 40.0,
-          width: 1000.0,
-          margin: EdgeInsets.symmetric(horizontal: 10.0),
-          */
-    );
-  } */
 }
