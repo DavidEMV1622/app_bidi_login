2063d9a2e549843402496e82911b25ba9d48966b
david <davidemv1622@gmail.com>
2023-10-26 15:29:25 -0500

    diseño de la pantalla del usuario logeado

--- lib/src/pages/Logeado.dart
+++ lib/src/pages/Logeado.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: file_names
 import 'package:app_credibanco_login/src/widgets/dropBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:iconify_flutter/icons/bi.dart';
 
 import '../colors/colors.dart';
 import '../utils/TextFormatter.dart';
+import '../widgets/buttons.dart';
 
 class LogeadoPage extends StatefulWidget {
   const LogeadoPage({super.key});
@@ -18,16 +20,19 @@ class _LogeadoPageState extends State<LogeadoPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    String simboloDolar = "\$";
+
     return Scaffold(
       appBar: AppBar(
         
         actions: [IconButton(onPressed: () {
           
-        }, icon: Icon(Icons.menu, size: 30, color: CustomColors.colorNegro))],
+        }, icon: const Icon(Icons.menu, size: 30, color: CustomColors.colorNegro))],
         title: Row(children: [
-          const Iconify(
-            Bi.person_circle,
-            color: CustomColors.colorNegro,
+          const CircleAvatar(
+            radius: 25,
+            backgroundColor: CustomColors.colorVerdePantanoClaro,
+            child: Text(""),
           ),
 
           const SizedBox(
@@ -37,12 +42,11 @@ class _LogeadoPageState extends State<LogeadoPage> {
           textoEtiquetaTwo("Hola, David", 20)
         ]),
         backgroundColor: CustomColors.colorVerdePantano,
-        elevation: 0,
+        elevation: 0.0,
       ),
       body: ListView(
         children: [
           Container(
-            height: size.height * 0.4,
             width: size.width,
             decoration: const BoxDecoration(
               borderRadius: BorderRadius.only(
@@ -50,11 +54,118 @@ class _LogeadoPageState extends State<LogeadoPage> {
                 bottomRight: Radius.circular(50)), 
               color:  CustomColors.colorVerdePantano,
             ),
-            child: Text("HOLA"),
+            child: Padding(
+              padding:const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 28,
+                  ),
+
+                  Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.end, children: [textoEtiquetaTwo(simboloDolar, 30), textoEtiquetaTwo("500.000", 40)],),
+                  
+                  textoEtiquetaTwo("Disponible", 15),
+
+                  const SizedBox(
+                    height: 36,
+                  ),
+
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceAround,children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundColor: CustomColors.colorVerdePantanoClaro, //<-- SEE HERE
+                          child: IconButton(
+                            icon: SvgPicture.asset( 
+                              "assets/icons/send.svg",
+                              width: 15 ,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+
+                        const SizedBox(
+                          height: 15,
+                        ),
+
+                        textoEtiquetaTwo("Enviar", 16),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundColor: CustomColors.colorVerdePantanoClaro, //<-- SEE HERE
+                          child: IconButton(
+                            icon: SvgPicture.asset( 
+                              "assets/icons/withdraw.svg",
+                              width: 15,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+
+                        const SizedBox(
+                          height: 15,
+                        ),
+
+                        textoEtiquetaTwo("Retirar", 16),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundColor: CustomColors.colorVerdePantanoClaro, //<-- SEE HERE
+                          child: IconButton(
+                            icon: SvgPicture.asset( 
+                              "assets/icons/receive.svg",
+                              width: 15,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+
+                        const SizedBox(
+                          height: 15,
+                        ),
+
+                        textoEtiquetaTwo("Recibir", 16),
+                      ],
+                    )
+
+                  ],),
+                  const SizedBox(
+                    height: 45,
+                  ), 
+                ],
+              ),
+            ),
           ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: textoEtiquetaTwo("Movimientos", 16),
+              ),
+
+              BotonLink(
+                textoLink: "Ver Todos",
+                colorText: CustomColors.colorGris_1,
+                onPressed: () {
+                  
+                },
+              ),
+            ],
+          )
         ],
       ),
-      bottomNavigationBar: barNavigation(page: true),
+      bottomNavigationBar: const barNavigation(page: true),
     );
   }
 }
