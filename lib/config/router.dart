a9702730d0a39445dc7d26d7b31d9d0bef4cf59d
david <davidemv1622@gmail.com>
2023-10-26 16:17:41 -0500

    cambios en las pantallas con el dropbar

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -21,7 +21,7 @@ import '../src/pages/avisos_page.dart';
 import '../src/pages/my_pocket.dart';
 
 final routes = GoRouter(
-  initialLocation: '/loginPage', /* Al iniciar la aplicacion, se 
+  initialLocation: '/logeadoPage', /* Al iniciar la aplicacion, se 
                                 ejecuta primero esta ruta */
   /* "routes" manejo de cada subruta */
 
