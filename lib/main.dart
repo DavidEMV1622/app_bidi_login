56c9e861c074686ad9bd26492d062a22014a8e44
david <davidemv1622@gmail.com>
2023-10-30 16:36:04 -0500

    Agregaado un post de prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_credibanco_login/src/Back-end/Token.dart';
 import 'package:app_credibanco_login/src/app.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,39 @@ import 'package:flutter/material.dart';
 
 
 
-void main() {
+/* void main() {
 
   runApp(const MyApp());
 
+} */
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ejemplo de solicitud POST'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              //postData(); // Llama a la función para realizar la solicitud POST
+              getTokenLogin('adminbidi@yopmail.com', 'Colombia.4',);
+            },
+            child: Text('Enviar POST'),
+          ),
+        ),
+      ),
+    );
+  }
 }
