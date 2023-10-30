f28ccd14846da07f94420c900f13b2b3ba69842e
david <davidemv1622@gmail.com>
2023-10-30 09:45:39 -0500

    Agregado una base para el consumo REST

---
+++ lib/src/Back-end/Token.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+ 
+ getTokenLogin(String user, String password){
+    
+    final String apiUrl = 'http://ec2-3-101-148-204.us-west-1.compute.amazonaws.com:8080/user/auth';
+
+    Future<void> postData() async {
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'username': 'adminbidi@yopmail.com',
+        'password': 'Colombia.4',
+      }),
+    );
+
+     if (response.statusCode == 200) {
+      // Si la solicitud POST es exitosa (código de estado 201 Created)
+      print('Solicitud POST exitosa');
+      print('Respuesta: ${response.body}');
+    } else {
+      // Si la solicitud POST falla, puedes manejar el error aquí
+      print('Error en la solicitud POST. Código de estado: ${response.statusCode}');
+    }
+
+  }
+ }
--- lib/src/common/inputValidate.dart
+++ lib/src/common/inputValidate.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: file_names
 // ---------- Menipular cada estructura de validación ----------
 // Funcion para validar la estructura del rfc
+
 import 'package:flutter/services.dart';
 import 'enumValidate.dart';
-import 'package:http/http.dart' as http;
 
 // Funcion para validar la estructura del nombre
 validateName(String name) {
@@ -57,23 +57,6 @@ class ValidateFormulations {
       default: // si no se da ninguno de los casos, no se manda una longitud
         return null;
     }
-  }
-
-  static getTokenLogin(String user, String password){
-
-
-    Future<void> postData() async {
-    final response = await http.post(
-      Uri.parse(apiUrl),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(<String, String>{
-        'title': 'Título del post',
-        'body': 'Cuerpo del post',
-      }),
-    );
-
   }
 
   // Funcion para manejar el tipo de caracter del input
